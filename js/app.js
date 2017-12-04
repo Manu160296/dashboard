e20aa825e9a986e6d195394be0dfa7920b82f6e0
Manu160296 <fmanuela499@gmail.com>
2017-12-04 10:22:05 -0500

    mejorando indentacion

--- js/app.js
+++ js/app.js
@@ -7,36 +7,33 @@ window.addEventListener('load', function() {
   var navigator = document.querySelector('.navigator');
 
   // agregando evento click a la barra de navegacion principal
-    navigator.addEventListener('click', showCharts);
-    function showCharts(event) {
-      console.log('hola')
-      if (event.target.getAttribute('value') === 'teachers') {
-        globalData.classList.remove('show');
-        globalData.classList.add('hide');
-      } else if (event.target.getAttribute('value') === 'overview')
-        globalData.classList.remove('hide');
-        globalData.classList.add('show');
-    }
+  navigator.addEventListener('click', showCharts);
+  function showCharts(event) {
+    if (event.target.getAttribute('value') === 'teachers') {
+      globalData.classList.remove('show');
+      globalData.classList.add('hide');
+    } else if (event.target.getAttribute('value') === 'overview')
+      globalData.classList.remove('hide');
+    globalData.classList.add('show');
+  }
 
-    // evento para los filtros:
-    var PLACE = '';
-    var CODE = '';
-    var SPRINT = '';
-    var students = {};
-    var placeFilter = document.querySelector('.place-filter');
-    var codeFilter = document.querySelector('.code-filter');
-    var sprintFilter = document.querySelector('.sprint-filter');
+  // evento para los filtros:
+  var PLACE = '';
+  var CODE = '';
+  var SPRINT = '';
+  var students = {};
+  var placeFilter = document.querySelector('.place-filter');
+  var codeFilter = document.querySelector('.code-filter');
+  var sprintFilter = document.querySelector('.sprint-filter');
 
-    codeFilter.addEventListener('change', showNumber);
+  codeFilter.addEventListener('change', showNumber);
 
-    // funcion del evento change:
-    function showNumber(event) {
-      PLACE = placeFilter.value;
-      CODE = this.value;
-      students = data[PLACE][CODE]['students'];
-      console.log(students)
-
-      // agregando datos:
+  // funcion del evento change:
+  function showNumber(event) {
+    PLACE = placeFilter.value;
+    CODE = this.value;
+    students = data[PLACE][CODE]['students'];
+    // agregando datos:
 
     // estudiantes activos por sede :
     var enrollmentStudents = document.querySelector('.enrollment-students');
@@ -53,7 +50,7 @@ window.addEventListener('load', function() {
     jediMasterRatings.textContent = jediMasterPoints(PLACE, CODE);
 
     var studentsSatisfaction = document.querySelector('.students-satisfaction');
-    studentsSatisfaction.textContent = satisfied(PLACE,CODE);
+    studentsSatisfaction.textContent = satisfied(PLACE, CODE);
 
     var netPromoterScore = document.querySelector('.nps');
     netPromoterScore.textContent = npsOfSprints(PLACE, CODE);
@@ -62,11 +59,10 @@ window.addEventListener('load', function() {
     promotersPercentage.textContent = promoters(PLACE, CODE) + '%';
 
     var detractorsPercentage = document.querySelector('.detractors');
-  detractorsPercentage.textContent = detractors(PLACE, CODE) + '%';
+    detractorsPercentage.textContent = detractors(PLACE, CODE) + '%';
 
-  var pasivePercentage = document.querySelector('.pasive');
-  pasivePercentage.textContent = pasive(PLACE, CODE) + '%';
-
+    var pasivePercentage = document.querySelector('.pasive');
+    pasivePercentage.textContent = pasive(PLACE, CODE) + '%';
 
 
     function totalActive(place, code, activeOrNotActive) {
@@ -87,107 +83,101 @@ window.addEventListener('load', function() {
     }
 
     // FUNCION QUE CALCULA EL PORCENTAJE DESERCION:
-  function desertionPercentage(place, code) {
-    var totalStudents = totalActive(place, code, 'active') + totalActive(place, code, 'notActive');
-    return parseFloat(((totalActive(place, code, 'notActive') / totalStudents) * 100).toFixed(1)) ;
-  }
-
-  // funcion que saca el promedio de los puntajes de los profesores   (todos los sprints)
-function TeachersPoints(place, code) {
-  var listOfCodes = data[place];
-  // var arrayPrueba= []
-  var sum = 0;
-  var promocion = listOfCodes[code];
-  var students = promocion.students;
-  var ratings = promocion.ratings;
-  for (var i = 0 ; i < ratings.length ; i++) {
-  // arrayPrueba.push(ratings[i].teacher)  ;
-    sum += ratings[i].teacher;
-  }
-  var average = sum / ratings.length;
-  return average.toFixed(1);
-}
-
-// funcion que saca el promedio de los puntajes de los jedi-master   (todos los sprints)
-
-function jediMasterPoints(place, code) {
-  var listOfCodes = data[place];
-  var sum = 0;
-  var promocion = listOfCodes[code];
-  var students = promocion.students;
-  var ratings = promocion.ratings;
-  for (var i = 0 ; i < ratings.length ; i++) {
-    sum += ratings[i].jedi;
-  }
-  var average = sum / ratings.length;
-  return average.toFixed(1);
-}
-
-// para calcular la satisfaccion de los estudiantes :
-
-
-function satisfied(place,code) {
-  var ratings = data[place][code].ratings;
-  var sumPercent= 0;
-    for (var i = 0; i < ratings.length; i++) {
-      sumPercent = sumPercent + ratings[i]['nps']['promoters'];
+    function desertionPercentage(place, code) {
+      var totalStudents = totalActive(place, code, 'active') + totalActive(place, code, 'notActive');
+      return parseFloat(((totalActive(place, code, 'notActive') / totalStudents) * 100).toFixed(1)) ;
     }
 
-    var averageSatisfied = sumPercent/ratings.length
-    return averageSatisfied.toFixed(2);
-  }
+    // funcion que saca el promedio de los puntajes de los profesores   (todos los sprints)
+    function TeachersPoints(place, code) {
+      var listOfCodes = data[place];
+      // var arrayPrueba= []
+      var sum = 0;
+      var promocion = listOfCodes[code];
+      var students = promocion.students;
+      var ratings = promocion.ratings;
+      for (var i = 0 ; i < ratings.length ; i++) {
+        // arrayPrueba.push(ratings[i].teacher)  ;
+        sum += ratings[i].teacher;
+      }
+      var average = sum / ratings.length;
+      return average.toFixed(1);
+    }
 
-  // funcion que calcula el net promoter score :
-  function npsOfSprints(place, code) {
-    var listOfCodes = data[place];
-    var anwersContainer = [];
-    var sum = 0;
-    var totalAnswers = 0;
-    var promocion = listOfCodes[code];
-    var students = promocion.students;
-    var ratings = promocion.ratings;
-    for (var i = 0 ; i < ratings.length ; i++) {
+    // funcion que saca el promedio de los puntajes de los jedi-master   (todos los sprints)
+
+    function jediMasterPoints(place, code) {
+      var listOfCodes = data[place];
+      var sum = 0;
+      var promocion = listOfCodes[code];
+      var students = promocion.students;
+      var ratings = promocion.ratings;
+      for (var i = 0 ; i < ratings.length ; i++) {
+        sum += ratings[i].jedi;
+      }
+      var average = sum / ratings.length;
+      return average.toFixed(1);
+    }
+
+    // para calcular la satisfaccion de los estudiantes :
+
+
+    function satisfied(place, code) {
+      var ratings = data[place][code].ratings;
+      var sumPercent = 0;
+      for (var i = 0; i < ratings.length; i++) {
+        sumPercent = sumPercent + ratings[i]['nps']['promoters'];
+      }
+
+      var averageSatisfied = sumPercent / ratings.length;
+      return averageSatisfied.toFixed(2);
+    }
+
+    // funcion que calcula el net promoter score :
+    function npsOfSprints(place, code) {
+      var listOfCodes = data[place];
+      var anwersContainer = [];
+      var sum = 0;
+      var totalAnswers = 0;
+      var promocion = listOfCodes[code];
+      var students = promocion.students;
+      var ratings = promocion.ratings;
+      for (var i = 0 ; i < ratings.length ; i++) {
       // para promoters
-      anwersContainer.push(ratings[i].nps.promoters - ratings[i].nps.detractors);
+        anwersContainer.push(ratings[i].nps.promoters - ratings[i].nps.detractors);
+      }
+      for (var j = 0 ; j < anwersContainer.length;j++) {
+        sum += anwersContainer[j];
+      }
+      return sum / anwersContainer.length;
     }
-    for (var j = 0 ; j < anwersContainer.length;j++) {
-      sum += anwersContainer[j];
+
+    // funcion que calula promoters :
+    function promoters(place, code) {
+      var sum = 0;
+      var array = data[place][code].ratings;
+      for (var i = 0 ; i < array.length ; i++) {
+        sum += data[place][code].ratings[i].nps.promoters;
+      }
+      return sum / array.length;
     }
-    return sum / anwersContainer.length;
+
+    function detractors(place, code) {
+      var sum = 0;
+      var array = data[place][code].ratings;
+      for (var i = 0 ; i < array.length ; i++) {
+        sum += data[place][code].ratings[i].nps.detractors;
+      }
+      return sum / array.length;
+    }
+
+    function pasive(place, code) {
+      var sum = 0;
+      var array = data[place][code].ratings;
+      for (var i = 0 ; i < array.length ; i++) {
+        sum += data[place][code].ratings[i].nps.passive;
+      }
+      return sum / array.length;
+    }
   }
-
-  // funcion que calula promoters :
-function promoters(place, code) {
-  var sum = 0;
-  var array = data[place][code].ratings;
-  for (var i = 0 ; i < array.length ; i++) {
-    sum += data[place][code].ratings[i].nps.promoters;
-  }
-  return sum / array.length;
-}
-
-function detractors(place, code) {
-    var sum = 0;
-    var array = data[place][code].ratings;
-    for (var i = 0 ; i < array.length ; i++) {
-      sum += data[place][code].ratings[i].nps.detractors;
-    }
-    return sum / array.length;
-  }
-
-  function pasive(place, code) {
-    var sum = 0;
-    var array = data[place][code].ratings;
-    for (var i = 0 ; i < array.length ; i++) {
-      sum += data[place][code].ratings[i].nps.passive;
-    }
-    return sum / array.length;
-  }
-
-
-
-    }
-
-
-
 });
